6b1f9aaa46289e89a0ff267de08770ccdb29e96c
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-11 16:47:36 -0300

    Calculando área

---
+++ ex011.py
@@ -0,0 +1,5 @@
+largura = float(input('Digite a largura da parede em metros: '))
+altura = float(input('Digite a altura da parede em metros: '))
+area = largura * altura
+quantidadetinta = area/2
+print('A área é: {}m2\nQuantidade de tinta: {}L'.format(area, quantidadetinta))

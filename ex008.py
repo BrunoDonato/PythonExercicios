fb98a2e71dfbc4846f86960760dd22c16a435e68
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-11 16:22:19 -0300

    Convertendo metros em cm e mm

---
+++ ex008.py
@@ -0,0 +1,4 @@
+metros = float(input('Digite um valor em Metros: '))
+cm = metros * 100
+mm = metros * 1000
+print('{} metros em centimetros é igual a: {}cm\n{} metros em milimetros é igual a: {}mm'.format(metros, cm, metros, mm))

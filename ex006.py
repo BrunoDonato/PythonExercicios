f10a94c66c258d84af3bbd35dcb109d71073c495
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-11 16:10:26 -0300

    Calculando dobro, triplo e raiz quadrada

---
+++ ex006.py
@@ -0,0 +1,7 @@
+import math
+
+num = int(input('Digite um número: '))
+dobro = num * 2
+triplo = num * 3
+raiz = math.sqrt(num)
+print('O dobro de {} é: {}\nO triplo de {} é: {}\nA raiz quadrada de {} é: {}'.format(num, dobro, num, triplo, num, raiz))

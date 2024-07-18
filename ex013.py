f7866f8b45b388e2be6b44e5a6f9225917c22df3
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-18 13:26:39 -0300

    Alteração na fórmula do aumento e no print

--- ex013.py
+++ ex013.py
@@ -1,3 +1,3 @@
-salario = float(input('Digite o salário: '))
-novosalario = salario + (salario*0.15)
-print('O novo salário com aumento de 15% é igual a: {}'.format(novosalario))
+salario = float(input('Digite o salário: R$'))
+novosalario = salario + (salario * 15/100)
+print('O novo salário com aumento de 15% é igual a: R${:.2f}'.format(novosalario))

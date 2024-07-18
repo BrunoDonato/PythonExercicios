ca1be2ef393aa91f579bb0a1054d55889e61f88b
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-18 13:20:05 -0300

    Alteração na fórmula do desconto e no print

--- ex012.py
+++ ex012.py
@@ -1,3 +1,3 @@
-preco = float(input('Digite o preço do produto: '))
-precodesconto = preco - (preco*0.05)
-print('O novo preço com desconto é igual a: {}'.format(precodesconto))
+preco = float(input('Digite o preço do produto: R$'))
+precodesconto = preco - (preco * 5/100)
+print('O produto que custava R${}, com um desconto de 5% vai custar R${}'.format(preco, precodesconto))

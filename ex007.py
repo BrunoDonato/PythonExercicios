6b69a365fd14274bd311272566d8721d55086ae3
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-18 12:32:20 -0300

    Coloquei para exibir o resultado com apenas uma casa decimal

--- ex007.py
+++ ex007.py
@@ -1,4 +1,4 @@
 nota1 = float(input('Informe a primeira nota: '))
 nota2 = float(input('Informe a segunda nota: '))
 media = (nota1 + nota2)/2
-print('Nota 1: {}\nNota2: {}\nA média das notas é: {}'.format(nota1, nota2, media))
+print('Nota 1: {:.1f}\nNota 2: {:.1f}\nA média das notas é: {:.1f}'.format(nota1, nota2, media))

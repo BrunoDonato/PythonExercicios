863272c4ecbf1e3f666032b5bac36d80386c205f
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-22 16:32:38 -0300

    Conversão de temperaturas

---
+++ ex014.py
@@ -0,0 +1,3 @@
+celsius = float(input('Informe a temperatura em Graus Celsius: '))
+faren = (celsius * 1.8) + 32
+print('{} graus Celsius corresponde à {:.1f} graus Fahrenheit.'.format(celsius, faren))

daaae3cdc36bd142c34d72b126d7ee8188734632
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-22 18:27:06 -0300

    (Utilizando Biblioteca) Mostrando apenas a parte inteira de um número

--- ex016.py
+++ ex016.py
@@ -1,4 +1,4 @@
 from math import trunc
 num = float(input('Digite qualquer número real: '))
 inteira = trunc(num)
-print('A parte inteira do número {} é {}'.format(num, inteira))
+print('A parte inteira do número informado {} é {}'.format(num, inteira))

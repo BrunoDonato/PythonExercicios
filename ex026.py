5d6c9695c6b069521b6b291a89cee6aa99e40dcd
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-07-30 18:25:51 -0300

    Primeira e ultima ocorrência de uma string

---
+++ ex026.py
@@ -0,0 +1,5 @@
+frase = str(input('Digite uma frase: ')).lower().strip()
+
+print('A letra A aparece {} vezes na frase'.format(frase.count('a')))
+print('A letra A aparece pela primeira vez na posição {}'.format(frase.find('a')+1))
+print('A letra A aparece pela ultima vez na posição {}'.format(frase.rfind('a')+1))

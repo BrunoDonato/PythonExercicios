d7f514330c08f3032bba749d1b02dc5dee2f0cf2
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-08-01 17:54:37 -0300

    Analisando se 3 retas formam um triângulo ou não

---
+++ ex035.py
@@ -0,0 +1,10 @@
+print('-=-' * 18)
+print('Analisando se 3 retas formam um triângulo ou não! ')
+print('-=-' * 18)
+r1 = float(input('Informe o comprimento da primeira reta: '))
+r2 = float(input('Informe o comprimento da segunda reta: '))
+r3 = float(input('Informe o comprimento da terceira reta: '))
+if (r1+r2) > r3 and (r1+r3) > r2 and (r2 + r3) > r1:
+    print('As 3 retas FORMAM um triângulo!')
+else:
+    print('As 3 retas NÃO FORMAM um triângulo!')

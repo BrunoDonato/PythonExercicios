50e0677f0712a211489540f32d6c08ef1fbb3f30
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-08-28 20:34:08 -0300

    (for) Contagem regressiva

---
+++ ex046.py
@@ -0,0 +1,6 @@
+import emoji
+from time import sleep
+for c in range(10, -1, -1):
+    print(c)
+    sleep(1)
+print(emoji.emojize("FELIZ ANO NOVO!:tada::clinking_glasses::sparkles:", language='alias'))

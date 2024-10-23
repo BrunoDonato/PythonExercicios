232d56b0453649fb9aae152a5e293b55c240da38
Bruno Amaral Carvalho Donato <brunoamaralsbt10@gmail.com>
2024-10-23 15:03:54 -0300

    Verificando se as pessoas são maiores ou menores de idade (for)

---
+++ ex054.py
@@ -0,0 +1,12 @@
+from datetime import date
+anoatual = date.today().year
+totalmaior = 0
+totalmenor = 0
+for c in range(1, 8):
+    ano = int(input('Informe o ano de nascimento da {}ª pessoa: '.format(c)))
+    idade = anoatual - ano
+    if idade < 18:
+        totalmenor += 1
+    else:
+        totalmaior += 1
+print('No total, tivemos {} menores de idade, e {} pessoas maiores de idade.'.format(totalmenor, totalmaior))
